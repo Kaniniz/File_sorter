df8c65c8620f199d1bb45037f53b753f9367fc9a
Kaniniz <majt1211@gmail.com>
2025-07-16 19:30:15 +0200

    It actually grabs the inputed filepath now and uses it in the dir command

--- main.c
+++ main.c
@@ -2,10 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
-void main() {
-    char *file_path = "C:/User/majt1/pictures";
-    printf("attempting to copy from: %s", file_path);
-    char *dir_cmd = "dir ";
+int main() {
+    char file_path[100];
+    
+    printf("Which folder should do you want to list?\n");
+    fgets(file_path, sizeof(file_path), stdin);
+    if (file_path == NULL) {
+        printf("Failed to get input");
+        return 1;
+    }
+    printf("attempting to list contents in folder %s\n", file_path);
+    
+    char dir_cmd[100];
+    strcpy(dir_cmd, "dir \"");
     strcat(dir_cmd, file_path);
-    system("dir C:/User/majt1/pictures");
+    strcat(dir_cmd, "\" /OD /TC");
+    printf("Command: %s\n", dir_cmd);
+
+    
+    
+    system(dir_cmd);
+    
+    
+    return 0;
 }
