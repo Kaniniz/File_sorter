f91ab68b5250dda8bd1b1ccf83d0c35e3221efb5
Kaniniz <majt1211@gmail.com>
2025-07-18 18:20:25 +0200

    Can get the date and file name

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main() {
         printf("Failed to get input");
         return 1;
     }
-
+    
     printf("Where should the copys go?\n");
     fgets(destination_path, sizeof(destination_path), stdin);
     if (destination_path == NULL) {
@@ -20,7 +20,9 @@ int main() {
         return 1;
     }
     printf("attempting to copy contents of %s\nTo the destination of: %s\n", source_path, destination_path);
+    
 
+    //Create the dir command to use in the cli, /OD sorts the list oldest file first, /TC makes it sort after the date the file was created.
     char dir_cmd[PATH_MAX];
     strcpy(dir_cmd, "dir \"");
     strcat(dir_cmd, source_path);
@@ -35,13 +37,35 @@ int main() {
         printf("Error executing dir command!\n");
         return -1;
     }
+
     
-    int i = 0;
-    printf("%s\n", cmd_output);
+    char *date;
+    char *name;
+    
+    
     while (fgets(file, PATH_MAX, cmd_output) != NULL)   {
-        printf("Files: %s", file);
-        i++;
-        printf("%i\n", strlen(file));
+        if (strchr(file, ' ') == file || strchr(file, '\\') == file) {
+            continue;
+        }
+        
+        //Grab the name of the file listed
+        //TODO: Doesn't account for whitespace in file names
+        name = strrchr(file, ' ');
+        //remove a leading whitespace in the name
+        if (name == NULL || name == " .."){
+            continue;
+        }
+        name++;
+        
+        //Grab the date the file was created
+        date = strtok(file, "-");
+        printf("Date: %s\nname: %s", date, name); 
+        
+        //Complete the target folder file path, if folders already exist does nothing
+        //TODO: get this part to work. system(mkdir C:\dir\path\name\) works, but getting the full path name is fucking impossible with how shit the string manipulations are in C.
+        
+
+
     }
 
 
@@ -50,7 +74,7 @@ int main() {
         printf("Error during pclose!");
         return -1;
     }
-    printf("%s\n%s\n", source_path, destination_path);
+    //printf("%s\n%s\n", source_path, destination_path);
     
     return 0;
 }
