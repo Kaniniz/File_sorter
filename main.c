a833ad254dbdac6d789a34938b90f073c5ab526b
Kaniniz <majt1211@gmail.com>
2025-07-18 13:08:52 +0200

    quick git push

--- main.c
+++ main.c
@@ -3,39 +3,54 @@
 #include <string.h>
 
 int main() {
-    char file_path[100];
-    
-    printf("Which folder should do you want to list?\n");
-    fgets(file_path, sizeof(file_path), stdin);
-    if (file_path == NULL) {
+    char source_path[PATH_MAX];
+    char destination_path[PATH_MAX];
+
+    printf("Which folder do you want to copy?\n");
+    fgets(source_path, sizeof(source_path), stdin);
+    if (source_path == NULL) {
         printf("Failed to get input");
         return 1;
     }
-    printf("attempting to list contents in folder %s\n", file_path);
-    
-    char dir_cmd[100];
+
+    printf("Where should the copys go?\n");
+    fgets(destination_path, sizeof(destination_path), stdin);
+    if (destination_path == NULL) {
+        printf("Failed to get input");
+        return 1;
+    }
+    printf("attempting to copy contents of %s\nTo the destination of: %s\n", source_path, destination_path);
+
+    char dir_cmd[PATH_MAX];
     strcpy(dir_cmd, "dir \"");
-    strcat(dir_cmd, file_path);
+    strcat(dir_cmd, source_path);
     strcat(dir_cmd, "\" /OD /TC");
-    printf("Command: %s\n", dir_cmd);
 
-    FILE *fp;
+    FILE *cmd_output;
     int status;
-    char path[PATH_MAX];
+    char file[PATH_MAX];
 
-    fp = popen(dir_cmd, "r");
-    if (fp == NULL) {
+    cmd_output = popen(dir_cmd, "r");
+    if (cmd_output == NULL) {
         printf("Error executing dir command!\n");
         return -1;
     }
+    
+    int i = 0;
+    printf("%s\n", cmd_output);
+    while (fgets(file, PATH_MAX, cmd_output) != NULL)   {
+        printf("Files: %s", file);
+        i++;
+        printf("%i\n", strlen(file));
+    }
 
-    while (fgets(path, PATH_MAX, fp) != NULL)
-    printf("Files: %s", path);
 
-    status = pclose(fp);
+    status = pclose(cmd_output);
     if (status == -1) {
         printf("Error during pclose!");
         return -1;
     }
+    printf("%s\n%s\n", source_path, destination_path);
+    
     return 0;
 }
